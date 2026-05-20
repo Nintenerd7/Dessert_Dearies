4142271aa2b40fdaf68ee20a5adf27897d9b3314
ニンテン <awsomestronhero@gmail.com>
2026-05-20 12:10:00 +0100

    Ball weight and scoreboard has been added
    I added some drag to the ball to not fall down to fast and have found the correct balance of jump force that doesn't go off the screen. In addition to adding a score board to add some sort of progress.

--- Assets/Scripts/MINIGAMES/Keepy uppy/Player_Controller.cs
+++ Assets/Scripts/MINIGAMES/Keepy uppy/Player_Controller.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player_Controller : MonoBehaviour
 {
-    float BounceForce = 15; //controls the force of the ball
+    float BounceForce = 10; //controls the force of the ball
     public Rigidbody2D _rigidbody; //rigidbody physics that adds force to the ball
     bool canBounce = false; //controls how many times you click the trigger collision 
+    public TMP_Text ScoreText; //Reference to text mesh pro element in the UI canvas
+    int ScoreCount; //Score count stores the value of the score parameter 
 
     void Update()//updates sequences in every frame. 
     {
         if (Input.GetMouseButtonDown(0) && canBounce) //if you press on the circle 
         {
             BounceButton(); // call bounce button logic
+            AddScore(10);//Call add score, Parameter adds 10 score points for each tap or mouse click
         } //end if
     }
     
@@ -37,6 +41,11 @@ public class Player_Controller : MonoBehaviour
     public void BounceButton() //handling the bounce force and the rigidbody force logic 
     {
             _rigidbody.AddForce(new Vector3(0, BounceForce), ForceMode2D.Impulse); //rigidbody adds force to bounce force value. 
+    }//end method 
 
+    public void AddScore(int score) // Add score method is used to add a value to the scoreboard hud. 
+    {
+      ScoreCount += score; //score parameter is assigned to the score count 
+      ScoreText.text = ScoreCount.ToString(); // score count is converted to string within the text mesh pro text
     }//end method 
 }
